b99c7eb7fd146e0058788617dc7cf42895b7ce2c
Gilad Levko <giladlevko@gmail.com>
2026-04-27 14:55:07 -0400

    Started CPP and finished the C course! Now I will commit the CPP projects to another repositery for better file management!

---
+++ cpp_new_features.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace :: std;
+
+long long fact(int n){
+    long long f = 1;
+    for (int i = 1; i<=n; i++){f *= i;}
+    return f;
+}
+
+int main(void){
+    cout << "this program uses some features not in C89\n";
+    for (auto i = 0; i < 16; i++){
+        cout << "factorial of " << i << " is " << fact(i)<<endl;
+    }
+    cout << "thats all folks!" << endl;
+}
